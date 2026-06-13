003bda16ee60278e3e841280825f8aa6e2f43428
longisrad <long.israd@gmail.com>
2026-06-13 18:07:35 +0700

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-# Copy file config của bạn vào trunk/.config
-cp build.config padavan-ng/trunk/.config
 
-# Nếu bạn muốn chắc chắn các thiết lập được nạp, hãy chạy lệnh này:
+# Di chuyển vào thư mục trunk
 cd padavan-ng/trunk
+
+# Lệnh khởi tạo chính xác theo Product ID từ file 2068.png
+fakeroot ./build_firmware_modify NEWIFI-D2
+
+# Sau đó mới copy file config của bạn
+cp -f ../../build.config .config
+
+# Cập nhật cấu hình
 make oldconfig
